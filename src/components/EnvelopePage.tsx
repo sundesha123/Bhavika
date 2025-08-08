7b36447d4f902e6d9e349fb82a72c27c188f3347
sundesha123 <pankajsundesha9@gmail.com>
2025-08-08 19:54:24 +0530

    Initial commit

--- src/components/EnvelopePage.tsx
+++ src/components/EnvelopePage.tsx
@@ -206,7 +206,7 @@ const EnvelopePage = ({ onNext }) => {
             animate={{ opacity: 1 }}
             transition={{ delay: 0.5 }}
           >
-            To my Dearest Bhavika💕
+            To my Dearest Payal💕
           </motion.p>
         </motion.div>
 
